a8cb9f344d4d41c930b976988ad95770645205af
stephipie <stephi.pietschmann@gmail.com>
2024-10-08 16:32:24 +0200

    modified data_visualization with pandas libary

--- data_visualization/data_visualization.py
+++ data_visualization/data_visualization.py
@@ -1,5 +1,7 @@
+# import libaries matplotlib and pandas
 import matplotlib.pyplot as plt
 from matplotlib import pyplot as plt
+import pandas as pd
 
 # Example-data: Numbers from 0 to 9 and their squares and cubes
 x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -15,11 +17,25 @@ plt.title('Square and cubic numbers from 0 to 9')
 plt.xlabel('number')
 plt.ylabel('cubes and squares')
 
-# show legend
+# Show legend
 plt.legend()
 
-# print png
+# Print png
 plt.savefig('data.visualization.png')
 
-# show diagramm
+# Show diagramm
 plt.show()
+
+# Create a simple Panda DataFrame
+data = {
+    "numbers": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    "squares": [i**2 for i in x],
+    "cubes": [i**3 for i in x]
+
+}
+
+# load data into a DataFrame object:
+df = pd.DataFrame(data)
+
+# save diagramm
+df.to_csv('number.csv', sep='\t', encoding='utf-8')
